a022c43c7be033b6209470ca3f74afcd12da0aad
Didac Bigorda <didacbigorda@gmail.com>
2019-08-14 01:09:24 +0200

    Add test page

---
+++ test.md
@@ -0,0 +1,9 @@
+---
+layout: page
+title: Test
+permalink: /test/
+---
+
+<script src="{{ site.baseurl }}/assets/js/index_bundle.js"><script>
+
+<div id="react-root"></div>
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.resolve(__dirname, 'react-dist'),
+    path: path.resolve(__dirname, './assets/js'),
     filename: 'index_bundle.js'
   },
   plugins: [
