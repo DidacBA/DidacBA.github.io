5a138401a2154d075248393ce819f8250ae13ff8
Didac Bigorda <didacbigorda@gmail.com>
2019-08-14 01:41:29 +0200

    Add babel plugin for classes. Add counter test react component to Test page

--- react-app/index.js
+++ react-app/index.js
@@ -2,11 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  handleClick = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  };
+
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <>
+        <div>
+          {this.state.count}
+        </div>
+        <button onClick={this.handleClick}>Increment counter</button>
+      </>
     )
   }
 }
