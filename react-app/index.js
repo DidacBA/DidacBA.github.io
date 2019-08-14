51d36f5dd6cdb240ddde8ccb5eebb09456c9f3d6
Didac Bigorda <didacbigorda@gmail.com>
2019-08-14 19:12:30 +0200

    Test for document fetching and parsing

--- react-app/index.js
+++ react-app/index.js
@@ -6,11 +6,19 @@ class App extends React.Component {
     count: 0,
   }
 
+  reactify() {
+    document.body.innerHTML = 'Hola';
+  }
+
   handleClick = () => {
     this.setState(({ count }) => ({
       count: count + 1
     }));
   };
+
+  getWebsite = () => {
+    getWebAsync('https://didacbigorda.com');
+  }
 
   render() {
     return (
@@ -19,9 +27,27 @@ class App extends React.Component {
           {this.state.count}
         </div>
         <button onClick={this.handleClick}>Increment counter</button>
+        <button onClick={this.reactify}>REACTIFY</button>
+        <button onClick={this.getWebsite}>Get Website</button>
       </>
     )
   }
+}
+
+function getWebAsync(website) {
+  fetch(website)
+    .then((response) => {
+      return response.text()
+    })
+    .then((html) => {
+      let parser = new DOMParser();
+
+      let doc = parser.parseFromString(html, "text/html");
+      console.log(doc.querySelector('.post-list'));
+    })
+    .catch((err) => {  
+      console.log('Failed to fetch page: ', err);  
+  });
 }
 
 ReactDOM.render(
