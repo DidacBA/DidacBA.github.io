2761e97d7edf3754add4f59d7e154565ec50562e
Didac Bigorda <didacbigorda@gmail.com>
2019-08-19 19:59:30 +0200

    Update to use hooks

--- react-app/index.js
+++ react-app/index.js
@@ -1,58 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import PostsList from './components/PostsList';
 import SearchBar from './components/SearchBar';
 import api from './api/api';
 
-class App extends React.Component {
-  state = {
-    unfilteredPosts: null,
-    posts: null,
-  }
+function App() {
+  const [posts, setPosts] = useState(null);
+  const [unfilteredPosts, setUnfilteredPosts] = useState(null);
 
-  handleSearch = (regExp) => {
+  const handleSearch = (regExp) => {
     // Condition to avoid constant fetch calls
     // Searches after first are performed over results of first call
 
-    if (this.state.posts === null) {
+    if (posts === null) {
       api.getNodes('https://didacbigorda.com', '.post-list li')
         .then((postsList) => {
           const filteredPosts = postsList.filter((post) => {
             return post.querySelector('.post-link').innerText.match(regExp) || post.querySelector('.post-meta').innerText.match(regExp)
           })
-  
-          this.setState(({prevState}) => ({
-            unfilteredPosts: postsList, 
-            posts: filteredPosts,
-          }));
+
+          setPosts(filteredPosts);
+          setUnfilteredPosts(postsList);
         })
     } else {
-      const filteredPosts = this.state.unfilteredPosts.filter((post) => {
+      const filteredPosts = unfilteredPosts.filter((post) => {
         return post.querySelector('.post-link').innerText.match(regExp) || post.querySelector('.post-meta').innerText.match(regExp)
       })
 
-      this.setState(({prevState}) => ({
-        posts: filteredPosts,
-      }))
+      setPosts(filteredPosts);
     }
   }
 
-  resetSearch = () => {
+  const resetSearch = () => {
     // Reset to value other than null to avoid refetching resources in handleSearch
 
-    this.setState(({prevState}) => ({
-      posts: '',
-    }));
+    setPosts('');
   }
 
-  render() {
-    return (
-      <div className='home'>
-        < SearchBar search={this.handleSearch} resetSearch={this.resetSearch} />
-        { this.state.posts && <PostsList posts={this.state.posts} /> }
-      </div>
-    )
-  }
+  return (
+    <div className='home'>
+      < SearchBar search={handleSearch} resetSearch={resetSearch} />
+      { posts && <PostsList posts={posts} /> }
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
